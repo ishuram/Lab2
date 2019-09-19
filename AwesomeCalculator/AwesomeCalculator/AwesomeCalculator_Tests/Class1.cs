82b6f00bc563952cead6554beab6ab35801805da
ishuram <ishuram@gmail.com>
2019-09-19 16:49:50 -0400

    commit 2-after adding add testcases

---
+++ AwesomeCalculator/AwesomeCalculator/AwesomeCalculator_Tests/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeCalculator;
+using NUnit.Framework;
+
+namespace AwesomeCalculator_Tests
+{
+    [TestFixture]
+    class ClassTests
+    {
+        [Test]
+        public void GetAddition_Inputminus99andminus1_Returnsminus100()
+        {
+            //Arrage
+            double number1 = -99;
+            double number2 = -1;
+            double expectedResult = number1 + number2;
+            Calc testCalc = new Calc(number1, number2);
+            //Act
+            double actualResult = testCalc.GetAddition();
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+        [Test]
+        public void GetAddition_Input2point99andminus1point99_Returns1()
+        {
+            //Arrage
+            double number1 = 2.99;
+            double number2 = -1.99;
+            double expectedResult = number1 + number2;
+            Calc testCalc = new Calc(number1, number2);
+            //Act
+            double actualResult = testCalc.GetAddition();
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+        [Test]
+        public void GetAddition_Input999and32768_Returns33767()
+        {
+            //Arrage
+            double number1 = 999;
+            double number2 = 32768;
+            double expectedResult = number1 + number2;
+            Calc testCalc = new Calc(number1, number2);
+            //Act
+            double actualResult = testCalc.GetAddition();
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
